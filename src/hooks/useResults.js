80cfeaf72c6a78419b53ebdc7c1790b03cb6c58f
LucasKeiti28 <lucaskeiti28@gmail.com>
2019-12-03 18:22:27 -0300

    Using Custom Hooks useHooks.

---
+++ src/hooks/useResults.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { Alert } from "react-native";
+
+import yelp from "../services/api/yelp";
+
+export default () => {
+  // const [location, setLocation] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    searchApi("pizza");
+  }, []);
+
+  const searchApi = async searchTerm => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "san jose"
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      Alert.alert("Sorry, something went wrong", "Please, try again later.");
+    }
+  };
+
+  return [searchApi, results];
+};
--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -2,32 +2,11 @@ import React, { useState, useEffect } from "react";
 import { SafeAreaView, Text, Alert } from "react-native";
 
 import SearchBar from "../../components/SearchBar";
-import yelp from "../../services/api/yelp";
+import useResults from "../../hooks/useResults";
 
 export default function Search() {
   const [term, setTerm] = useState("");
-  // const [location, setLocation] = useState("");
-  const [results, setResults] = useState([]);
-
-  useEffect(() => {
-    searchApi(term);
-  }, []);
-
-  const searchApi = async searchTerm => {
-    console.log("Hello, World!");
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: "san jose"
-        }
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      Alert.alert("Sorry, something went wrong", "Please, try again later.");
-    }
-  };
+  const [searchApi, results] = useResults();
 
   return (
     <SafeAreaView>
